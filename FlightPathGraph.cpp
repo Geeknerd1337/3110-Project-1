5b540fe3c32be7eead6c82e04d85c9fda224c32a
Sheik13 <102391877+Sheik13@users.noreply.github.com>
2022-04-02 17:30:31 -0500

    Update FlightPathGraph.cpp

--- FlightPathGraph.cpp
+++ FlightPathGraph.cpp
@@ -9,18 +9,18 @@ void FlightPathGraph::readFile(){
 
 			//get starting city name
 			if (fileInputLine.find("From:") == 0) {
-				node = fileInputLine.substr(6, fileInputLine.length());
+				node = fileInputLine.substr(7, fileInputLine.length());
 			}
 
 			//get first connection city, and add to map
-			else if (fileInputLine.find("To:") == 0) {
-				connection = fileInputLine.substr(4, fileInputLine.length());
+			else if (fileInputLine.find("To  :") == 0) {
+				connection = fileInputLine.substr(7, fileInputLine.length());
 				addEdge(node, connection);
 			}
 
 			//get subsequent connection cities, and add to map
 			else {
-				connection = fileInputLine;
+				connection = fileInputLine.substr(7, fileInputLine.length());
 				addEdge(node, connection);
 			}
 		}
@@ -33,3 +33,4 @@ void FlightPathGraph::readFile(){
 void FlightPathGraph::addEdge(string node, string connection) {
 	edges[node].push_back(connection);
 }
+
