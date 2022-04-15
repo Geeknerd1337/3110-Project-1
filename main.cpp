0bef685e96ac5dcdbb215a296229d058baf59518
Sheik13 <102391877+Sheik13@users.noreply.github.com>
2022-04-15 14:14:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,8 @@ int main() {
     //Read in the local file
     graph.readFile();
 
-    //graph.routeSearch2("Winston-Salem, United States","Tel Aviv, Israel","Warsaw, Poland","Brisbane, Australia");
-    graph.breadthFirstSearch("Warsaw, Poland", "Tel Aviv, Israel");
-    //graph.processPath("Tel Aviv, Israel", "Brisbane, Australia");
-    //graph.printPath();
-    graph.printParents();
+    graph.routeSearch2("Winston-Salem, United States","Tel Aviv, Israel","Warsaw, Poland","Brisbane, Australia");
+   
 
     return 0;
 }
