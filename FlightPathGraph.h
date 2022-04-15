4fb69a141bb4e33c5b81f604538792239f6c2b6e
Sheik13 <102391877+Sheik13@users.noreply.github.com>
2022-04-15 14:14:06 -0500

    Update FlightPathGraph.h

--- FlightPathGraph.h
+++ FlightPathGraph.h
@@ -14,6 +14,8 @@ class FlightPathGraph
 private:
 	string fileInputLine;
 	ifstream flightPathFile;
+
+	vector<string> visitedInProcess;
 public:
 
 	void readFile();
@@ -26,19 +28,19 @@ public:
 
 	void printPath();
 
-	map<string, string> parents;
+	//map<string, string> parents;
+	map<string, vector<string>> parents;
 
 	vector<string> path;
 
-	void processPath(string start, string end);
+	void processPath(string start, string end, bool addEndToPath);
 
 	void routeSearch1(string start, string end, int num);
 
 	void routeSearch2(string start, string visit1, string visit2, string end);
 
 	void printParents();
-	
+
 	map<string, vector<string> > edges;
 
 };
-
